75c187d85166fb92bfde2e03d357b440c1d5d9c9
ibrahim ethem <21060671@stu.omu.edu.tr>
2022-12-25 19:52:41 +0300

    dosyalar eklendi

---
+++ lib/login.py
@@ -0,0 +1,33 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+import sys
+from login_ui import Ui_MainWindow
+from main import MainWİndow
+class MainnWindow(QMainWindow):
+    def __init__(self):
+        super(MainnWindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.main = MainWİndow()
+        self.ui.setupUi(self)
+        self.initUi()
+    def initUi(self):
+        self.show()
+        self.setWindowTitle("Alya Siha LOGİN")
+        self.ui.password_lineEdit.setText("admin")
+        self.ui.send_btn.clicked.connect(self.sorgula)
+
+    def sorgula(self) :
+        if(self.ui.password_lineEdit.text() == "admin"):
+            self.close()
+            self.main.showMaximized()
+        else:
+            self.ui.message_box_label.setText("Incorrect Password\nError Code :x3400")
+            
+def mainLOOP():
+    fas = QApplication(sys.argv)
+    win = MainnWindow()
+    sys.exit(fas.exec_())
+
+mainLOOP()
+
---
+++ lib/main.py
@@ -0,0 +1,20 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+
+from main_UI import Ui_MainWindow
+import sys
+
+class MainWİndow(QMainWindow) :
+    def __init__(self):
+        super(MainWİndow,self).__init__()
+        self.ui = Ui_MainWindow()
+        self.setWindowTitle("ALYA SİHA KONTROL PANELİ")
+        self.initUI()
+
+    def initUI(self):
+        self.ui.setupUi(self)
+        self.ui.Null1_btn.clicked.connect(lambda : self.ui.stackedWidget.setCurrentWidget(self.ui.Null_page))
+        self.ui.controller_btn.clicked.connect(lambda : self.ui.stackedWidget.setCurrentWidget(self.ui.controller_page))
+        
+
